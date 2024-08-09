d255620fbd3727a5bae045c174c05b3de16c3ab4
silvalorena <108538058+silvalorena@users.noreply.github.com>
2024-08-09 12:28:44 -0300

    bom

---
+++ script.js
@@ -0,0 +1,11 @@
+const caixaprincipal = document.querySelector(".caixa- principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
+const caixaresultado = document.querySelector(".caixa-resultado");
+const textoresultao = document.querySelector(".texto-resultado ");
+
+const perguntas = [
+
+
+
+]
